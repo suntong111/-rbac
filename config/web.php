dc74e1d7f05435ded86d5743499fd69e74f0ef4f
悲傷1 <592691858@qq.com>
2018-08-14 11:11:58 +0800

    write your meaaage

--- config/web.php
+++ config/web.php
@@ -53,6 +53,7 @@ $config = [
         */
     ],
     'params' => $params,
+
 ];
 
 if (YII_ENV_DEV) {
@@ -68,7 +69,10 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+    $config['modules']['admin']=[
+        'class' => 'app\modules\admin',
     ];
 }
 
---
+++ modules/controllers/PublicController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/14 0014
+ * Time: 11:00
+ */
+
+namespace app\modules\controllers;
+
+
+use yii\web\Controller;
+
+class PublicController  extends Controller
+{
+
+}
